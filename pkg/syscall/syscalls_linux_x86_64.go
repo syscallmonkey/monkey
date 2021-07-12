67ca374bc3db9d8e45dd7847cf00e9bba9972733
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2021-07-12 22:46:20 +0100

    Plug the manipulator into the Tracer, if any

--- pkg/syscall/syscalls_linux_x86_64.go
+++ pkg/syscall/syscalls_linux_x86_64.go
@@ -18,7 +18,34 @@ func ReadSyscallArg(regs syscall.PtraceRegs, argNo int) uint64 {
 		return regs.R8
 	case 5:
 		return regs.R9
+	// return value
+	case -2:
+		return regs.Rax
+	// syscall code
 	default:
 		return regs.Orig_rax
+	}
+}
+
+func WriteSyscallArg(regs *syscall.PtraceRegs, argNo int, value uint64) {
+	switch argNo {
+	case 0:
+		regs.Rdi = value
+	case 1:
+		regs.Rsi = value
+	case 2:
+		regs.Rdx = value
+	case 3:
+		regs.R10 = value
+	case 4:
+		regs.R8 = value
+	case 5:
+		regs.R9 = value
+	// return value
+	case -2:
+		regs.Rax = value
+	// syscall code
+	default:
+		regs.Orig_rax = value
 	}
 }
--- pkg/syscall/tracer.go
+++ pkg/syscall/tracer.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Tracer struct {
-	Pid     int
-	Counter *SyscallCounter
-	Out     io.Writer
+	Pid         int
+	Counter     *SyscallCounter
+	Out         io.Writer
+	Manipulator SyscallManipulator
 }
 
 func NewTracer(pid int, out io.Writer) *Tracer {
@@ -55,9 +56,44 @@ func (t *Tracer) Loop() {
 
 func (t *Tracer) HandleSyscallEntry(regs syscall.PtraceRegs) {
 	fmt.Fprintf(t.Out, FormatSyscallEntry(t.Pid, regs))
+	if t.Manipulator != nil {
+		code := ReadSyscallArg(regs, -1)
+		state := SyscallState{
+			Arg0:        ReadSyscallArg(regs, 0),
+			Arg1:        ReadSyscallArg(regs, 1),
+			Arg2:        ReadSyscallArg(regs, 2),
+			Arg3:        ReadSyscallArg(regs, 3),
+			Arg4:        ReadSyscallArg(regs, 4),
+			Arg5:        ReadSyscallArg(regs, 5),
+			SyscallCode: code,
+			SyscallName: GetSyscallName(code),
+			Pid:         t.Pid,
+		}
+		newState := t.Manipulator.HandleEntry(state)
+		if newState != state {
+			newRegs := regs
+			WriteSyscallArg(&newRegs, -1, newState.SyscallCode)
+			WriteSyscallArg(&newRegs, 0, newState.Arg0)
+			WriteSyscallArg(&newRegs, 1, newState.Arg1)
+			WriteSyscallArg(&newRegs, 2, newState.Arg2)
+			WriteSyscallArg(&newRegs, 3, newState.Arg3)
+			WriteSyscallArg(&newRegs, 4, newState.Arg4)
+			WriteSyscallArg(&newRegs, 5, newState.Arg5)
+			syscall.PtraceSetRegs(t.Pid, &newRegs)
+		}
+	}
 }
 
 func (t *Tracer) HandleSyscallExit(regs syscall.PtraceRegs) {
 	t.Counter.Inc(regs.Orig_rax)
 	fmt.Fprintf(t.Out, FormatSyscallExit(regs))
+	if t.Manipulator != nil {
+		ret := ReadSyscallArg(regs, -2)
+		newRet := t.Manipulator.HandleExit(ret)
+		if newRet != ret {
+			newRegs := regs
+			WriteSyscallArg(&newRegs, -2, newRet)
+			syscall.PtraceSetRegs(t.Pid, &newRegs)
+		}
+	}
 }
