a36b6420403ab441080ce4301a987b58e5eac13b
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2021-07-12 02:45:20 +0100

    Finish printing char *

--- pkg/syscall/tracer.go
+++ pkg/syscall/tracer.go
@@ -2,8 +2,37 @@ package syscall
 
 import (
 	"fmt"
+	"strings"
 	"syscall"
 )
+
+func ReadSyscallArg(regs syscall.PtraceRegs, arg int) uint64 {
+	switch arg {
+	case 0:
+		return regs.Rdi
+	case 1:
+		return regs.Rsi
+	case 2:
+		return regs.Rdx
+	case 3:
+		return regs.R10
+	case 4:
+		return regs.R8
+	case 5:
+		return regs.R9
+	default:
+		return regs.Orig_rax
+	}
+}
+
+func clen(n []byte) int {
+	for i := 0; i < len(n); i++ {
+		if n[i] == 0 {
+			return i
+		}
+	}
+	return len(n)
+}
 
 type Tracer struct {
 	Pid     int
@@ -25,7 +54,7 @@ func (t *Tracer) Loop() {
 	// handle the first syscall on its way out - the execve
 	syscall.PtraceGetRegs(t.Pid, &regs)
 	t.Counter.Inc(regs.Orig_rax)
-	PrintSyscall(regs)
+	t.PrintSyscall(regs)
 	fmt.Printf(" = %d\n", regs.Rax)
 	incall = true
 	// handle all the other syscalls
@@ -44,7 +73,7 @@ func (t *Tracer) Loop() {
 			break
 		}
 		if incall {
-			PrintSyscall(regs)
+			t.PrintSyscall(regs)
 			t.Counter.Inc(regs.Orig_rax)
 		} else {
 			ret := int64(regs.Rax)
@@ -56,5 +85,35 @@ func (t *Tracer) Loop() {
 			}
 		}
 		incall = !incall
+	}
+}
+
+func (t *Tracer) PrintSyscall(regs syscall.PtraceRegs) {
+	code := regs.Orig_rax
+
+	var argsText []string
+	for i, argType := range GetSyscallArgumentTypes(code) {
+		argsText = append(argsText, t.PrintSyscallArgument(argType, ReadSyscallArg(regs, i)))
+	}
+
+	fmt.Printf("%s(%v)", GetSyscallName(code), strings.Join(argsText, ", "))
+}
+
+func (t *Tracer) PrintSyscallArgument(argType string, val uint64) string {
+	switch argType {
+	case "int":
+		return fmt.Sprintf("%d", int(val))
+	case "const char __user *":
+		if val == 0 {
+			return "NULL"
+		}
+		var out []byte = make([]byte, 128)
+		_, err := syscall.PtracePeekData(t.Pid, uintptr(val), out)
+		if err != nil {
+			panic(err)
+		}
+		return string(out[:clen(out)])
+	default:
+		return fmt.Sprintf("%d", val)
 	}
 }
